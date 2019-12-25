1f5c110ab46899476b3402247fd33d805cec8dd1
DESKTOP-K6VQ42D\Qiu <838689269@qq.com>
2019-12-25 18:16:04 +0800

    修改端口返回json

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
@@ -1,28 +1,23 @@
-package com.qingcheng.controller;
+package com.qingcheng.controller.goods;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.qingcheng.pojo.goods.Brand;
 import com.qingcheng.service.goods.BrandService;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-/**
- * @author QiuMingJie
- * @date 2019-12-17 23:34
- * @Description
- */
 @RestController
 @RequestMapping("/brand")
 public class BrandController {
-
     @Reference
-    private BrandService brandService;
+    private  BrandService brandService;
 
-    @RequestMapping(value = "/findAll",method = RequestMethod.GET)
-    public List<Brand> findAll() {
+    @RequestMapping("/findAll")
+    public List<Brand> findAll(){
         return brandService.findAdd();
     }
+
+
 }
